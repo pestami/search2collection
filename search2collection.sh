d0e4bd5e77d4a314886f3667db32997039762aff
Mirek Pesta <mirekpesta@gmail.com>
2025-07-19 23:24:00 +0200

    missing files

---
+++ search2collection.sh
@@ -0,0 +1,8 @@
+!/bin/bash 
+
+echo ========VERSIONS 202/04/03===========================
+echo 
+echo 
+#=========================================================
+
+python3 /home/pi/RetroPie/roms/ports/search2collection/search2collection.py
