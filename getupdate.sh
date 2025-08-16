4f5927c0d0ca8d75983c2a0e05cfc07c303686c8
PatMat <51946906+pestami@users.noreply.github.com>
2025-08-16 13:30:52 +0100

    Tested on Pi4

--- getupdate.sh
+++ getupdate.sh
@@ -35,6 +35,9 @@ target03=/home/pi/RetroPie/roms/ports/search2collection/search2collection.py
 source04=s2c_search_rich.py
 target04=/home/pi/RetroPie/roms/ports/search2collection/s2c_search_rich.py
 
+source05=s2c_db.py
+target05=/home/pi/RetroPie/roms/ports/search2collection/s2c_db.py
+
 #===list of files in text file=====
 # wget -i update.txt
 
@@ -43,7 +46,7 @@ wget -O $target01 $rootsource$branch$source01
 wget -O $target02 $rootsource$branch$source02 
 wget -O $target03 $rootsource$branch$source03
 wget -O $target04 $rootsource$branch$source04
-
+wget -O $target05 $rootsource$branch$source05
 
 
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
