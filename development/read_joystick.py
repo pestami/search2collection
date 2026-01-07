138ace1a4a0426c8a5911b670efd1f0fe2b780bd
Mirek Pesta <mirekpesta@gmail.com>
2026-01-07 12:47:23 +0100

    .......

--- development/read_joystick.py
+++ development/read_joystick.py
@@ -10,41 +10,47 @@ import evdev  #apt install python3-evdev
 import os
 import time
 
+class joystic4kbd:
+#===============================================================================
+    def sentence_to_set(self, sentence):
+        # Remove punctuation and convert to lower case
+        sentence = ''.join(e for e in sentence if e.isalnum() or e.isspace()).lower()
+        # Split the sentence into words and convert to a set
+        word_set = set(sentence.split())
+        return word_set
 
-def sentence_to_set(sentence):
-    # Remove punctuation and convert to lower case
-    sentence = ''.join(e for e in sentence if e.isalnum() or e.isspace()).lower()
-    # Split the sentence into words and convert to a set
-    word_set = set(sentence.split())
-    return word_set
-
-
-def main():
-    devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
-    for device in devices:
-        print(f"Found DEVICE: {device.name}")
-        
-        setDeviceName=sentence_to_set(device.name.lower())
-        setControlers={'joystick','gamecontroller','gamepad','contoler'}
-        
-        
-        if setDeviceName&setControlers:  # Look for joystick devices
-            print(f"Gamepad: {device.name}")
+#===============================================================================
+    def PollJoystick(self):
+        devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+        for device in devices:
+            print(f"Found DEVICE: {device.name}")
             
-            for event in device.read_loop():
+            setDeviceName=self.sentence_to_set(device.name.lower())
+            setControlers={'joystick','gamecontroller','gamepad','contoler'}
+            
+            
+            if setDeviceName&setControlers:  # Look for joystick devices
+                print(f"Gamepad: {device.name}")
                 
-                
+                for event in device.read_loop():
+                    
+                    os.system('cls||clear')
+                    print('==READ JOYSTICK using evdf======')
+    
+    
+                    time.sleep(1)
+                    
+                    if event.type == evdev.ecodes.EV_ABS:
+                        print(f"Axis {event.code}: {event.value}")
+                        #break
+                    elif event.type == evdev.ecodes.EV_KEY:
+                        print(f"Button {event.code}: {'pressed' if event.value == 1 else 'released'}")
+                        #break
 
-
-                #time.sleep(1)
-                
-                if event.type == evdev.ecodes.EV_ABS:
-                    print(f"Axis {event.code}: {event.value}")
-                    #break
-                elif event.type == evdev.ecodes.EV_KEY:
-                    print(f"Button {event.code}: {'pressed' if event.value == 1 else 'released'}")
-                    #break
-
-
+#===============================================================================
 if __name__ == "__main__":
-    main()
+    
+    
+    MP_joystic4kbd=joystic4kbd()
+    
+    MP_joystic4kbd.PollJoystick()
---
+++ development/search2collection_kbd.sh
@@ -0,0 +1,8 @@
+!/bin/bash 
+
+echo ========VERSIONS 2026/01/03===========================
+echo 
+echo 
+#=========================================================
+
+python3 /home/pi/RetroPie/roms/ports/search2collection/search2collection_kbd.py
