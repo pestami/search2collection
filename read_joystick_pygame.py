5b162a36145e581c7c24151fc5ecc6301fc552e3
Mirek Pesta <mirekpesta@gmail.com>
2026-01-12 12:49:54 +0100

    Mapp keys on joystick

--- read_joystick_pygame.py
+++ read_joystick_pygame.py
@@ -1,14 +1,13 @@
 import pygame
 import sys
+import configparser
 
 #
 #===============================================================================
 class pygame_joystick:
     
-    #global my_debug
-    
-    #def __init__(self,**arg):
-        
+    #global my_debug    
+    #def __init__(self,**arg):        
     def __init__(self,**arg):
         
         self.my_debug=True
@@ -18,12 +17,30 @@ class pygame_joystick:
         pygame.init()
     
         # Initialize joystick module
-        pygame.joystick.init()
+        pygame.joystick.init()        
+        
+        if self.my_debug: print("===INITIALIZE=====")     
         
         
-        if self.my_debug: print("===INITIALIZE=====")
-      
-        
+
+
+    def load_ini_file(self,file_path):
+        config = configparser.ConfigParser()
+        config.read(file_path)
+    
+        if 'Buttons' in config:
+            buttons_config = config['Buttons']
+            ButtonUP = buttons_config.get('ButtonUP', '')
+            ButtonDOWN =  buttons_config.get('ButtonDOWN', '')
+            ButtonLEFT =  buttons_config.get('ButtonLEFT', '')
+            ButtonRIGHT =  buttons_config.get('ButtonRIGHT', '')
+            ButtonSTART =  buttons_config.get('ButtonSTART', '')
+    
+            return ButtonUP, ButtonDOWN, ButtonLEFT, ButtonRIGHT, ButtonSTART
+        else:
+            print("The file does not contain a 'Buttons' section.")
+            return None
+
 
     def poll_joystick(self,**arg):
         
@@ -45,41 +62,49 @@ class pygame_joystick:
         if self.my_debug: 
            print(f"Joystick name: {joystick.get_name()}")
         CMD=''
-        # Main loop
+        
+        
+        ButtonUP, ButtonDOWN, ButtonLEFT, ButtonRIGHT, ButtonSTART = self.load_ini_file('joystick_buttons.ini')
+        
+        if self.my_debug: 
+            print(f"ButtonUP: {ButtonUP}")
+            print(f"ButtonDOWN: {ButtonDOWN}")
+            print(f"ButtonLEFT: {ButtonLEFT}")
+            print(f"ButtonRIGHT: {ButtonRIGHT}")
+            print(f"ButtonSTART: {ButtonSTART}")
+        # ------------------Main loop--------------------------------------
         
         while True:
-            # Get events
-            for event in pygame.event.get():                
+            #---------Get events-----------------------------------------------
+            for event in pygame.event.get():               
 
-
-                                                 
                 if event.type == pygame.JOYBUTTONUP:
                     if self.my_debug: 
                            print(f"Button {event.button} released")
                            
-                    if event.button ==2:
+                    if event.button ==int(ButtonLEFT):
                         if self.my_debug: 
                            print("moved LEFT")
                         CMD="LEFT"
                         CMD="s"
                         
-                    if event.button ==1:
+                    if event.button ==int(ButtonRIGHT):
                         if self.my_debug: 
                            print("moved RIGHT")
                         CMD="RIGHT"
                         CMD="d"
                             
-                    if event.button ==3:
+                    if event.button ==int(ButtonUP):
                         if self.my_debug: 
                                print("moved UP")
                         CMD="UP"
                         CMD="e"
-                    if event.button ==0:
+                    if event.button ==int(ButtonDOWN):
                         if self.my_debug: 
                                 print("moved DOWN")
                         CMD="DOWN"
                         CMD="x"
-                    if event.button ==5:
+                    if event.button ==int(ButtonSTART):
                          if self.my_debug: 
                                 print("Released Button")
                          CMD="BUTTON"
@@ -97,7 +122,8 @@ class pygame_joystick:
                             # pygame.quit()
                             # sys.exit()
                             return CMD
-                    
+              #----END-----Get events----------------------------------------      
+        
         return CMD   
     
 #===============================================================================
@@ -105,6 +131,21 @@ if __name__ == "__main__":
     
     
     joy_kbd=pygame_joystick(my_debug=True)
+    
+    # Example usage
+    file_path = 'joystick_buttons.ini'
+    values = joy_kbd.load_ini_file(file_path)
+    
+    if values:
+        ButtonUP, ButtonDOWN, ButtonLEFT, ButtonRIGHT, ButtonSTART = values
+        print(f"ButtonUP: {ButtonUP}")
+        print(f"ButtonDOWN: {ButtonDOWN}")
+        print(f"ButtonLEFT: {ButtonLEFT}")
+        print(f"ButtonRIGHT: {ButtonRIGHT}")
+        print(f"ButtonSTART: {ButtonSTART}")     
+    
+    
+
     
     CMD=joy_kbd.poll_joystick()
     
