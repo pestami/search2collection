ab758d32763d6dfd0aadc8deb74d321b778f03ba
Mirek Pesta <mirekpesta@gmail.com>
2026-01-15 13:48:18 +0100

    Delete Virtual Keyboard Scripts

--- search2collection_kbd.sh
+++
@@ -1,8 +0,0 @@
-!/bin/bash 
-
-echo ========VERSIONS 2026/01/03===========================
-echo 
-echo 
-#=========================================================
-
-python3 /home/pi/RetroPie/roms/ports/search2collection/search2collection_kbd.py
